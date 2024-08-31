406e0b40e962aa43c9dc1f744f2444b9a747a6b6
Psuk0125 <69672032+Psuk0125@users.noreply.github.com>
2024-08-31 21:11:44 +0630

    Update server.js

--- Lister/server.js
+++ Lister/server.js
@@ -10,10 +10,10 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'sspark@919597',
-  database: 'library'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
