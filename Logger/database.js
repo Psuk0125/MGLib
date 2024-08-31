b6a631d2a4a545b50fbf4d7814aa401be3b10f71
Psuk0125 <69672032+Psuk0125@users.noreply.github.com>
2024-08-31 20:01:48 +0630

    Update database.js

--- Logger/database.js
+++ Logger/database.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
+require("dotenv").config(); // Load environment variables from .env file
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "sspark@919597",
-    database: "library"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
